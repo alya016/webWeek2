ae352c4cc4bd08a2e303c04be169bb3c5f05c56f
Aliya <aliya_ermahan@mail.ru>
2020-03-19 12:00:40 +0600

    upd

---
+++ app.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK="fa-check-circle";
+const Unsheck="fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
